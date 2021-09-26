923218e14f91c77de4ec3cb06e5ce5b54c9a5e6e
A S Sowrov Shikder <91294321+NOOB-BD-HACKER@users.noreply.github.com>
2021-09-27 01:28:30 +0300

    Update Remove.sh

--- Remove.sh
+++ Remove.sh
@@ -3,6 +3,6 @@ z="
 eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Mz$Nz$z$Oz$z$Pz$Qz$Rz$Sz$Tz$Uz$z$Vz$z$Wz$z$Xz$Yz$Zz$z$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$gz$pz$qz$rz$sz$tz$uz$vz$vz$wz$xz$yz$ABz$BBz$CBz$DBz$z$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$z$Wz$z$Vz$z$WBz$XBz$YBz$ZBz$aBz$ez$bBz$OBz$cBz$dBz$eBz$fBz$gBz$hBz$z$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$gz$pz$qz$rz$sz$tz$iBz$z$wz$xz$yz$ABz$BBz$CBz$DBz$z$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$z$Wz$z$Vz$z$WBz$XBz$YBz$ZBz$aBz$ez$bBz$OBz$cBz$dBz$eBz$fBz$jBz$kBz$lBz$z$Wz$z$Az$Bz$Cz$mBz$z$Xz$nBz$z$Az$Bz$Cz$mBz"
  
 
-'Done now type exit and open after 10 seconds'
+echo 'Done now type exit and open after 10 seconds' |lolcat 
 
 
